e95ddcf9846a9063284bc7bdcf4d3a250f279657
donno2048 <just4now666666@gmail.com>
2021-12-28 23:27:16 +0200

    add compatibility to compilation

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flaskwebgui import FlaskUI
+from pathlib import Path
+import sys
 app = Flask(__name__)
 ui = FlaskUI(app)
 @app.route('/')
-def index(): return open('index.html').read()
+def index(): return open(Path(getattr(sys, '_MEIPASS', Path.cwd())) / 'index.html').read()
 if __name__ == '__main__': ui.run()
